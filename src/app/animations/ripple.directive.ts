4ae1d69602c9f9ffe8bbe8e0d2d44ec80284bab2
weboey <583287168@qq.com>
2017-09-26 23:59:00 +0800

    css

--- src/app/animations/ripple.directive.ts
+++ src/app/animations/ripple.directive.ts
@@ -1,52 +1,72 @@
-import { Directive, ElementRef, HostListener,HostBinding, Input} from '@angular/core';
+import { Directive, ElementRef, HostListener,HostBinding, Input,NgZone} from '@angular/core';
 
 @Directive({
   selector: '[appRipple]'
 })
 export class RippleDirective {
 
-  constructor(el:ElementRef) {
-    //el.nativeElement.addEventListener('click', this.createRipple, false);
-  }
+  constructor(private el:ElementRef,private _ngZone: NgZone) {}
+
+  private _activeRipples = new Set<HTMLElement>();
 
   //host: {
   //  '(click)': 'createRipple($event.target)'
   //}
   //HostListener 是属性装饰器，用来为宿主元素添加事件监听。
+  _isMousedown:boolean=true;
 
   @HostBinding('class.a-ripple') isRippleStart: boolean=true;
 
   @HostListener('mousedown',['$event'])
-  createRipple(event) {
+  private createRipple(event) {
+    this._isMousedown=true;
     let target = event.target;
     var rect = target.getBoundingClientRect();
-    //let ripple = target.querySelector('.ripple');
-   // if (!ripple) {
-     var  ripple = document.createElement('div');
-      ripple.className = 'ripple';
+    let  ripple:HTMLElement = document.createElement('div');
+    ripple.className = 'ripple';
     let radius = this.calcuRippleRect(event.pageX,event.pageY,rect);
-     // ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height)*2.5 + 'px';
-      ripple.style.height = ripple.style.width = `${radius * 2}px`;
-      target.appendChild(ripple);
-   // }
+    ripple.style.height = ripple.style.width = `${radius * 2}px`;
+    target.appendChild(ripple);
     var top = event.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
     var left = event.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
     ripple.style.top = top + 'px';
     ripple.style.left = left + 'px';
     ripple.style.transform = 'scale(1)';
-
-    setTimeout(()=>{
-      target.removeChild(ripple);
-    },2000)
-
+    this._activeRipples.add(ripple);
+    this.runTimeoutOutsideZone(() => {
+      if (!this._isMousedown) {
+        this.fadeOutRipple(ripple)
+      }
+    }, 2000);
     return false;
   }
-  @HostListener('mouseup',['$event'])
-  removeRipple(event) {
 
+  @HostListener('mouseup',['$event'])  // removeRipple(event) {}
+  private onMouseup() {
+    this._isMousedown = false;
+    this._activeRipples.forEach(ripple => {
+      this.fadeOutRipple(ripple);
+    });
   }
 
-  calcuRippleRect(x: number, y: number, rect: ClientRect) {
+  private fadeOutRipple(rippleRef) {
+    if (!this._activeRipples.delete(rippleRef)) {
+      return;
+    }
+    let rippleEl = rippleRef;
+    rippleEl.style.transitionDuration = '780ms';
+    rippleEl.style.opacity = '0';
+
+    this.runTimeoutOutsideZone(() => {
+      rippleEl.parentNode!.removeChild(rippleEl);
+    }, 2000);
+  }
+
+  private runTimeoutOutsideZone(fn: Function, delay = 0) {
+    this._ngZone.runOutsideAngular(() => setTimeout(fn, delay));
+  }
+
+  private calcuRippleRect(x: number, y: number, rect: ClientRect) {
     const distX = Math.max(Math.abs(x - rect.left), Math.abs(x - rect.right));
     const distY = Math.max(Math.abs(y - rect.top), Math.abs(y - rect.bottom));
     return Math.sqrt(distX * distX + distY * distY);
